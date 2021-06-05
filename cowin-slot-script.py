84e931247b87753a48f468edeb612490e50c7946
sachin-bhikule <85405866+sachin-bhikule@users.noreply.github.com>
2021-06-05 21:59:24 +0530

    Additional Changes

--- cowin-slot-script.py
+++ cowin-slot-script.py
@@ -1,26 +1,27 @@
 import requests
 import time
 import datetime
+import os
 
 from playsound import playsound
 
-#By default this checks the slots for next day edit this date as per your convenience
-date1 = datetime.datetime.now() + datetime.timedelta(days=1)
-date = date1 .strftime('%d-%m-%Y')
+script_dir = os.path.dirname(__file__)
+rel_path = "cowin-slot-script.mp3"
+sound_path = os.path.join(script_dir, rel_path)
 
-#path for the sound
-path='D:/Vaccine Script/cowin-slot-script.mp3'
+#edit this date as per your convenience
+today = datetime.datetime.now() + datetime.timedelta(days=1)
+tomorrow = today .strftime('%d-%m-%Y')
 
 #City code
 dist=363
 
 URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(
-    dist, date)
+    dist, tomorrow)
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-
-
+flag=1
 def findAvailability():
         counter = 0
         result = requests.get(URL, headers=header)
@@ -35,12 +36,15 @@ def findAvailability():
                 print(parameter["vaccine"])
                 print(parameter["available_capacity"])
                 print(datetime.datetime.now())
-                playsound(path)
+                playsound(sound_path)
 
         if(counter == 0):
             return False
 
 
 while(findAvailability() != True):
+    if(flag==1):
+        print("The script is now running as soon as the slots will open you will get notified")
+        flag=0
     time.sleep(5)
     findAvailability()
